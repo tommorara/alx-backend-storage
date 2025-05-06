adbb71ba19061ee3c12f2ab88e58f0fb1ea534e2
Tom Morara <morara1920@gmail.com>
2025-05-06 22:08:32 +0300

    retrieve schools by topic

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+
+"""
+Task details:
+
+Write a Python function that returns all students sorted by average score:
+
+    - Prototype: `def top_students(mongo_collection)`
+    - mongo_collection will be the `pymongo` collection object
+    - The top must be ordered
+    - The average score must be part of each item returns with key =
+      `averageScore`
+"""
+
+
+def top_students(mongo_collection):
+    """Return all students sorted by average score."""
+    return mongo_collection.aggregate(
+        [
+            {
+                "$project": {
+                    "name": "$name",
+                    "averageScore": {"$avg": "$topics.score"},
+                }
+            },
+            {"$sort": {"averageScore": -1}},
+        ]
+    )
