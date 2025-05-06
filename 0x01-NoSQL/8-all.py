947bec65a9c6b0d12e88d4825e413e547cc2d606
Tom Morara <morara1920@gmail.com>
2025-05-06 22:06:40 +0300

    list all documents from a MongoDB collection using PyMongo

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+This module contains a single function that lists all documents in NoSQL
+collection.
+"""
+
+
+def list_all(mongo_collection):
+    """Lists all the documents in a collection."""
+
+    if mongo_collection is None:
+        return []
+
+    return mongo_collection.find({})
